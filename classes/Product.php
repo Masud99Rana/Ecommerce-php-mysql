2a87e3d28e4eb60cc9080786f0e14509837b5ce1
Masud99Rana <Masudrana917775@gmail.com>
2019-09-25 22:58:29 +0600

    Branded product added in homepage

--- classes/Product.php
+++ classes/Product.php
@@ -231,6 +231,35 @@ class Product
 
 		return $result;
 	}
+
+	public function latestFromIphone(){
+
+		$query = "SELECT * FROM tbl_product WHERE brandId = '4'  ORDER BY productId DESC LIMIT 1 ";
+		$result = $this->db->select($query);
+		return $result;
+	}
+
+	public function latestFromSamsung(){
+
+		$query = "SELECT * FROM tbl_product WHERE brandId = '2'  ORDER BY productId DESC LIMIT 1 ";
+		$result = $this->db->select($query);
+		return $result;
+	}
+
+	public function latestFromAcer(){
+
+		$query = "SELECT * FROM tbl_product WHERE brandId = '1'  ORDER BY productId DESC LIMIT 1 ";
+		$result = $this->db->select($query);
+		return $result;
+	}
+
+	public function latestFromCanon(){
+
+		$query = "SELECT * FROM tbl_product WHERE brandId = '3'  ORDER BY productId DESC LIMIT 1 ";
+		$result = $this->db->select($query);
+		return $result;
+	}
+
 }
 
 
